4b5d5c15f398b7b52afc81f2d6f9ab1850f9c8ac
Cynthia <Cynthia@Cynthia-PC>
2013-11-09 14:56:10 -0200

    Mudança de pacote.

---
+++ Projeto_Bank/src/org/ufpr/lpoo/projetobanco/Conta.java
@@ -0,0 +1,14 @@
+package org.ufpr.lpoo.projetobanco;
+
+
+public abstract class  Conta {
+    public boolean deposita(double valor){    
+   //o valor depositado deve ser positivo. Caso contrário o método retorna false
+        return true;        
+    }
+    public boolean saca(double valor){
+    //o valor sacado deve ser positivo. Caso contrário 
+    //o método retorna false. Mostrar mensagem na tela informando usuário.
+        return true;
+    }
+}
