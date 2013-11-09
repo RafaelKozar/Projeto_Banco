156af108e97b6a220dbbfd41e1b62a50b8e071fd
Cynthia <Cynthia@Cynthia-PC>
2013-11-09 14:55:32 -0200

    Mudança de pacote.

---
+++ Projeto_Bank/src/org/ufpr/lpoo/projetobanco/ContaCorrente.java
@@ -0,0 +1,18 @@
+package org.ufpr.lpoo.projetobanco;
+
+
+import org.ufpr.lpoo.projetobanco.Conta;
+
+
+
+
+public class ContaCorrente extends Conta{
+    public boolean saca(double valor){
+//        Antes de efetuar o saque deve-se verificar se o
+//valor sacado não ultrapassa o limite da conta. Ou seja, a conta poderá ficar negativa até o limite
+//estipulado na sua criação. Mostrar mensagem na tela informando o usuário.
+        return true;
+    }    
+    public void remunera(){
+    }
+}
